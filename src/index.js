bcb2104c03dec602e3a1c391270d30285ed55394
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2021-10-15 18:09:07 +0330

    Install mongodb and mongoose packages + Create basic projects route

--- src/index.js
+++ src/index.js
@@ -4,6 +4,15 @@ const app = express()
 
 const port = process.env.PORT || 8000
 
+app.get('/projects', (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
+    res.send({data: 'Hello World'})
+})
+
+app.get('/resume', (req, res) => {
+
+})
+
 app.listen(port, () => {
     console.log('Server up and running on port ' + port)
 })
