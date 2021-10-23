627dbbb059539867b56e70670fc44bbb3fea1324
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2021-10-23 19:41:21 +0330

    Remove options object from connect method based on update version 6.0.11 for mongoose

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,8 +1,3 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://127.0.0.1:27017/my-portfolio', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true
-})
+mongoose.connect('mongodb://127.0.0.1:27017/my-portfolio-api')
