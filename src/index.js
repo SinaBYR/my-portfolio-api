06b74a595d0dfd72d687c92427761bde1f96ff5b
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2021-10-23 19:42:09 +0330

    Require mongoose.js file to run mongoose server

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 const express = require('express')
 const projectsRouter = require('./routes/project')
+const cors = require('cors')
+
+// for running mongoose server
+require('./db/mongoose')
 
 const app = express()
 const port = process.env.PORT || 8000;
 
+
 app.use(express.json())
 app.use(projectsRouter)
-
+app.use(cors())
 
 app.listen(port, () => {
     console.log('Server up and running on port ' + port)
